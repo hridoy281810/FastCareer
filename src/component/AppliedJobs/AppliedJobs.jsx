2bf33bd6b9d98f29576bb4153e5a65ae6980d84f
hridoy281810 <hridoy01724281810@gmail.com>
2023-04-10 23:19:06 +0600

    Featured Jobs section added

--- src/component/AppliedJobs/AppliedJobs.jsx
+++ src/component/AppliedJobs/AppliedJobs.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const AppliedJobs = () => {
+
+
     return (
         <div>
            <h1>Applied Jobs</h1> 
--- src/component/Statistics/Statistics.jsx
+++ src/component/Statistics/Statistics.jsx
@@ -1,6 +1,51 @@
 import React from 'react';
 
 const Statistics = () => {
+    const assignmentData =[
+        {
+            id: "1c45s",
+            name: "new year new me",
+            mark: 60
+        },
+        {
+            id: "2a45s",
+            name: "g3-architects ",
+            mark: 60
+        },
+        {
+            id: "3b46s",
+            name: "legal-solution-resources",
+            mark: 60
+        },
+        {
+            id: "4d47s",
+            name: "Basic Javascript Problem",
+            mark: 46
+        },
+        {
+            id: "5e48s",
+            name: "Geometry Genius",
+            mark: 52
+        },
+        {
+            id: "6f49s",
+            name: "AI Universe API and ES6",
+            mark: 56
+        },
+        {
+            id: "7g59s",
+            name: "Make Quiz Hero a great quiz",
+            mark: 60
+        },
+        {
+            id: "8h69s",
+            name: "Knowledge cafe",
+            mark: 60
+        }
+    ] 
+    {
+        
+    }
     return (
         <div>
             <h1>Statistics
